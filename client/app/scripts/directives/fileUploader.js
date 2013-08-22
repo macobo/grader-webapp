3d54aca5344b100aec29263958363fb6d478f3d1
Karl-Aksel Puulmann <macobo@ut.ee>
2013-08-22 21:16:31 +0300

    fileuploader

--- client/app/scripts/directives/fileUploader.js
+++ client/app/scripts/directives/fileUploader.js
@@ -2,21 +2,56 @@
 
 angular.module('graderApp')
   .directive('fileUploader', function () {
-    var linker = function(scope, elem, attr) {
+
+    var linker = function(scope, elem, attrs) {
       var dropParagraph = elem.find('p');
       var browseButton = elem.find('a');
-      
+      var fileInput = elem.find('input');
 
+
+      var processDragOverOrEnter = function(event) {
+        console.log(event);
+        event.preventDefault();
+        event.originalEvent.dataTransfer.effectAllowed = 'copy';
+        return false;
+      };
+      elem.bind('dragover', processDragOverOrEnter);
+      elem.bind('dragenter', processDragOverOrEnter);
+
+      var setFile = function(file) {
+        scope.file = file;
+        dropParagraph.text(file.name);
+      };
+
+      scope.file = null;
+
+      browseButton.click(function() {
+        fileInput.click();
+      });
+
+      fileInput.bind('change', function(event) {
+        setFile(this.files[0]);
+        scope.$apply();
+      })
+
+      elem.bind('drop', function(event) {
+        event.preventDefault();
+        setFile(event.originalEvent.dataTransfer.files[0]);
+      });
     };
+
     return {
       restrict: 'A',
       template: '<form id="file-upload" enctype="multipart/form-data">' +
-                  '<div id="drop">' +
-                    '<p class="inputtext">DROP FILE HERE</p><a>Browse</a>' +
+                  '<div class="drop">' +
+                    '<p class="inputtext">DROP FILE HERE</p>' +
+                    '<a class="uploadbutton">Browse</a>' +
                     '<input type="file" name="file" />' +
                   '</div>' +
+                  '<div ng-transclude></div>' +
                 '</form>',
       replace: true,
+      transclude: true,
       link: linker
     };
   });
